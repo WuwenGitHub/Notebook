481fc572aad2a3bbf8f9f2820f525491520f5b56
WuwenGitHub <1466358244@qq.com>
2018-04-12 23:08:52 +0800

    Update GuestLoggingInfo.java

--- note/Java/GuestLoggingInfo.java
+++ note/Java/GuestLoggingInfo.java
@@ -1,6 +1,14 @@
+package test;
+
+import java.util.Date;
+
 public class GuestLoggingInfo implements java.io.Serializable 
 { 
-    private Date loggingDate = new Date(); 
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Date loggingDate = new Date(); 
     private String uid; 
     private transient String pwd; 
     
@@ -8,9 +16,30 @@ public class GuestLoggingInfo implements java.io.Serializable
     { 
         uid = "guest"; 
         pwd = "guest"; 
-    } 
-    public String toString() 
+    }
+    
+    GuestLoggingInfo(String usr, String password) 
     { 
-        //same as above 
-     } 
+        uid = usr; 
+        pwd = password; 
+    } 
+    
+    public String toString() {
+		String password = null;
+		if (pwd == null) {
+			password = "NOT SET";
+		} else {
+			password = pwd;
+		}
+		return "login info: \n   " + "user: " + uid + "\n   logging date : " + loggingDate.toString()
+				+ "\n   password: " + password;
+	}
+
+	public static void main(String args[]) {
+		GuestLoggingInfo guestInfo = new GuestLoggingInfo();
+		System.out.println(guestInfo.toString());
+		
+		GuestLoggingInfo guestInfo2 = new GuestLoggingInfo("MIKE", "MECHANICS");
+		System.out.println(guestInfo2.toString());
+	}
 } 
